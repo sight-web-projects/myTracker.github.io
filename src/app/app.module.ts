47c3bb9e8f26d0200091fc815f51b475f38dbcb6
Kabali Michael <fitzerkisakyamukama03@gmail.com>
2020-07-09 21:48:08 +0300

    add modules learning faith projects finance reading family friends

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LearningModule } from './learning/learning.module';
+import { FaithModule } from './faith/faith.module';
+import { ProjectsModule } from './projects/projects.module';
+import { FinanceModule } from './finance/finance.module';
+import { ReadingModule } from './reading/reading.module';
+import { FamilyModule } from './family/family.module';
+import { FriendsModule } from './friends/friends.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LearningModule,
+    FaithModule,
+    ProjectsModule,
+    FinanceModule,
+    ReadingModule,
+    FamilyModule,
+    FriendsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
